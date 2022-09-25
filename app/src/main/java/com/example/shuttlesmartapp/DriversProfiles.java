8a7da9f8dc731efb6bed6b8ec81f82eef1a594aa
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2022-09-25 18:27:39 +0200

    Updated UI , Added button functionality + Created Drivers Profiles screen

---
+++ app/src/main/java/com/example/shuttlesmartapp/DriversProfiles.java
@@ -0,0 +1,14 @@
+package com.example.shuttlesmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriversProfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers_profiles);
+    }
+}
--- app/src/main/java/com/example/shuttlesmartapp/MainActivity.java
+++ app/src/main/java/com/example/shuttlesmartapp/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnShuttleSchedules = findViewById(R.id.btnShuttleSchedules);
+        Button btnDriverProfiles = findViewById(R.id.btnDriverProfiles);
         Button btnLogout = findViewById(R.id.btnLogout);
 
         btnShuttleSchedules.setOnClickListener(new View.OnClickListener() {
@@ -25,17 +26,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnDriverProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDriverProfiles();
+            }
+        });
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "The logout button has been clicked!", Toast.LENGTH_LONG).show();
-                System.exit(0);
+               // System.exit(0);
             }
         });
     }
 
     public void openShuttleSchedules() {
         Intent intent = new Intent(this, ShuttleSchedules.class);
+        startActivity(intent);
+    }
+
+    public void openDriverProfiles() {
+        Intent intent = new Intent(this, DriversProfiles.class);
         startActivity(intent);
     }
 }
