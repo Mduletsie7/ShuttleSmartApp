8912dd4979133fa9ffec70a779bdbb25ec53d1f8
Mduletsie7 <mdu.letsie7@gmail.com>
2022-11-21 21:48:26 +0200

    Updated UI ,Removed Driver profiles screen in exchange for About Team section

--- app/src/main/java/com/example/shuttlesmartapp/DriversProfiles.java
+++ app/src/main/java/com/example/shuttlesmartapp/DriversProfiles.java
@@ -2,6 +2,7 @@ package com.example.shuttlesmartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -11,5 +12,9 @@ public class DriversProfiles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers_profiles);
+
+        WebView webView = (WebView) findViewById(R.id.AboutUs);
+        webView.setBackgroundColor(Color.rgb(255, 255, 255));
+        webView.loadUrl("file:///android_asset/aboutUs/index.html");
     }
 }
